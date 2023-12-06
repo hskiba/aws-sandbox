e70d05154cab1b6d4e91f8a1d7a94e30a834b9a1
Henry Skiba <henry.skiba@frgrisk.com>
2023-12-06 16:03:19 +0800

    initialize terraform

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "hskiba-sandbox-terraform-state"
+    key            = "tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "terraform-state-lock"
+  }
+}
+
+provider "aws" {
+  region              = var.region
+  allowed_account_ids = [var.account_id]
+}
---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "account_id" {
+  description = "AWS Account ID"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS Region"
+  default     = "us-east-2"
+  type        = string
+}
